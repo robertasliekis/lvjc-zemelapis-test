a4851c0d9b22a9ad07d85c183ee76de9cd3fef24
MSI <robertasliekis@gmail.com>
2022-02-07 22:07:49 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import { connect } from "react-redux";
 import activePoints from "./info/texts";
@@ -9,20 +9,57 @@ import "./App.css";
 
 function App() {
   const [clickedIcon, setClickedIcon] = useState(null);
+  const [infoWindowTop, setInfoWindowTop] = useState(0);
+  const [contentWidth, setContentWidth] = useState(0);
+
+  const iconRef = useRef([]);
+  const contentRef = useRef();
 
   useEffect(() => {}, []);
 
+  const selectIcon = (key) => {
+    const position = window.getComputedStyle(iconRef.current[key]).getPropertyValue("top");
+    const screenWidth = contentRef.current.getBoundingClientRect().width;
+
+    setContentWidth(screenWidth);
+
+    const iconTopPosition = parseInt(position.substring(0, position.length - 2));
+
+    if (key === 10) {
+      if (screenWidth > 1000) {
+        setInfoWindowTop(window.innerHeight / 1.3);
+      } else if (screenWidth > 800) {
+        setInfoWindowTop(window.innerHeight / 2);
+      } else if (screenWidth > 600) {
+        setInfoWindowTop(window.innerHeight / 2.5);
+      } else {
+        setInfoWindowTop(50);
+      }
+    } else {
+      setInfoWindowTop(iconTopPosition);
+    }
+
+    setClickedIcon(key);
+  };
+
   return (
     <div className="App">
-      <div className="content-wrapper">
+      <div className="content-wrapper" ref={contentRef}>
         <img className="map" src={require("./images/map_icons.jpg")} alt="" />
         <div className="icons-wrapper">
           {activePoints.map((activePoint, key) => (
-            <img onClick={() => setClickedIcon(key)} className={`icon icon-${activePoint.image}`} key={key} src={require(`./images/icons/${activePoint.image}.png`)} alt="" />
+            <img
+              onClick={() => selectIcon(key)}
+              ref={(ref) => (iconRef.current[key] = ref)}
+              className={`icon icon-${activePoint.image}`}
+              key={key}
+              src={require(`./images/icons/${activePoint.image}.png`)}
+              alt=""
+            />
           ))}
         </div>
         {clickedIcon !== null ? (
-          <div className="info-window">
+          <div className="info-window" style={{ top: contentWidth > 420 ? infoWindowTop : "10px" }}>
             <div onClick={() => setClickedIcon(null)} className="btn-close"></div>
             <div className="text">
               <h3>{activePoints[clickedIcon].title}</h3>
